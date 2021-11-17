a5bb3f845bd7d4968d91f1f773bc306e64aea400
swathibhat <swathi.bhat01@gmail.com>
2021-11-17 10:13:03 +0530

    Converting for loops to streams

--- java/matrix/src/main/java/Matrix.java
+++ java/matrix/src/main/java/Matrix.java
@@ -1,28 +1,23 @@
+import java.util.Arrays;
+
 class Matrix {
 
-    int[][]  matrix;
+    int[][] matrix;
 
     Matrix(String matrixAsString) {
-        String[] rows = matrixAsString.split("\n");
-        matrix = new int[rows.length][rows[0].split(" ").length];
+        matrix =  Arrays.stream(matrixAsString.split("\n")).map(r ->
+        {
+            return Arrays.stream(r.split(" ")).mapToInt(Integer::parseInt).toArray();
 
-        for(int i =0 ; i<rows.length;i++){
-            String[] columns = rows[i].split(" ");
-            for(int j =0 ; j<columns.length ; j++){
-                matrix[i][j] = Integer.parseInt(columns[j]);
-            }
-        }
+        }).toArray(int[][]::new);
+
     }
 
     int[] getRow(int rowNumber) {
-        return matrix[rowNumber-1];
+        return matrix[rowNumber - 1];
     }
 
     int[] getColumn(int columnNumber) {
-        int[] requiredColumn = new int[matrix.length];
-        for(int i = 0 ; i<matrix.length ; i++){
-            requiredColumn[i] = matrix[i][columnNumber-1];
-        }
-        return requiredColumn;
+        return Arrays.stream(matrix).mapToInt(m -> m[columnNumber - 1]).toArray();
     }
 }
