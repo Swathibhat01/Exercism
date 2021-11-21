655e9eb01b810b6ed7be5831053bf95cd3548fad
swathibhat <swathi.bhat01@gmail.com>
2021-11-21 15:01:06 +0530

    Adds raindrop solution

---
+++ java/raindrops/src/main/java/RaindropConverter.java
@@ -0,0 +1,20 @@
+class RaindropConverter {
+
+    String convert(int number) {
+        StringBuilder sb = new StringBuilder();
+        if(number%3==0){
+            sb.append("Pling");
+        }
+        if(number%5==0){
+            sb.append("Plang");
+        }
+        if(number%7==0){
+            sb.append("Plong");
+        }
+        if (sb.length()==0){
+            sb.append(number);
+        }
+        return String.valueOf(sb);
+    }
+
+}
