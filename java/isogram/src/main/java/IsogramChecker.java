274c9ceaddff37ac7933f09aa1d803e1d77207d3
swathibhat <swathi.bhat01@gmail.com>
2021-11-22 12:31:10 +0530

    Adds other ways to solve Isogram Checker

--- java/isogram/src/main/java/IsogramChecker.java
+++ java/isogram/src/main/java/IsogramChecker.java
@@ -1,24 +1,60 @@
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
-
 class IsogramChecker {
 
     boolean isIsogram(String phrase) {
-        String phraseWithoutHyphensAndSpace = phrase.replaceAll("[\\s-]+", "");
-        System.out.println(phraseWithoutHyphensAndSpace);
-        return isIsogramHelperWithSets(phraseWithoutHyphensAndSpace);
-    }
+        Set<Character> setOfUniqueCharInPhrase = new HashSet<>();
+        return phrase.replaceAll("[\\s-]+","").chars()
+            .mapToObj(c->(char)c)
+            .map(Character::toLowerCase)
+            .allMatch(setOfUniqueCharInPhrase::add);
+
+//        String phraseWithoutHyphensAndSpace =phrase.replaceAll("[\\s-]+" ,"");
+//        return isIsogramHelperWithoutExtraSpace(phraseWithoutHyphensAndSpace.toLowerCase());
+}
 
     private boolean isIsogramHelperWithSets(String phrase) {
+        int lengthOfPhrase = phrase.length();
         Set setOfUniqueCharInPhrase = new HashSet();
-        for (int i = 0; i < phrase.length(); i++) {
-            boolean add = setOfUniqueCharInPhrase.add(phrase.toUpperCase().charAt(i));
-            if (!add) {
+        for(int i = 0 ; i<phrase.length();i++){
+            boolean add = setOfUniqueCharInPhrase.add(phrase.charAt(i));
+            if(!add){
                 return false;
             }
         }
         return true;
     }
 
+    private boolean isIsogramHelperWithArray(String phrase){
+        int[] arrayOfAlphabetScore = new int[26];
+        for(int i =0 ; i<phrase.length();i++){
+         if(arrayOfAlphabetScore[phrase.charAt(i)-'a'] == 0) {
+             arrayOfAlphabetScore[phrase.charAt(i) - 'a']++ ;
 
-}
+         }
+         else
+             return false;
+             }
+        return true;
+        }
+
+    private boolean isIsogramHelperWithoutExtraSpace(String phrase){
+     char[] charArrayOfPhrase = phrase.toCharArray();
+        Arrays.sort(charArrayOfPhrase);
+        for(int i = 0 ; i < charArrayOfPhrase.length-1;i++){
+            if (charArrayOfPhrase[i]==charArrayOfPhrase[i+1]){
+                return false;
+            }
+        }
+        return true;
+
+    }
+
+
+
+    }
+
+
+
+
